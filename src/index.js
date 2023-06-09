4c02b8c197ba328e167404d1bc34a5495a9b5ddb
Ievgennii <simonovievgenii@gmail.com>
2023-06-09 21:06:26 +0300

    add comments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-
 import { persistR, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -16,3 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>
 );
+
+
+//<Provider store={store}> ... </Provider>: Оборачивает <App /> в <Provider>. 
+//<Provider> предоставляет доступ к Redux store для всех компонентов, которые находятся внутри него. 
+//Здесь Redux store передается в качестве свойства store
